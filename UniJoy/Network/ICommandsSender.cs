e06612438702bedc07064b3c29e0227f3c69845a
avielkar <avraham.elkar@gmail.com>
2020-04-09 23:07:31 +0300

    Adding commands sendet interface

---
+++ UniJoy/Network/ICommandsSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniJoy.Network
+{
+    public interface ICommandsSender
+    {
+        bool TrySendCommand(string commandName, string commandValue);
+
+        bool TryStart();
+
+        bool TryStop();
+    }
+}
