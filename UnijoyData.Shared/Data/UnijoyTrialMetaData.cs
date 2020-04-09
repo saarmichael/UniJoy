99468346851929c945c21ba45c978d67a53cfcbb
avielkar <avraham.elkar@gmail.com>
2020-04-09 15:39:29 +0300

    Adding UnijoyMedtaData class to be seerialized and sent to the unity engine

---
+++ UnijoyData.Shared/Data/UnijoyTrialMetaData.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Assets.Data;
+
+namespace Assets.Network.Retrievers
+{
+    [JsonObject]
+    public class UnijoyTrialMetaData : ITrialData
+    {
+        public int Source { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public List<float> X { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public List<float> Y { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public List<float> Z { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public List<float> RX { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public List<float> RY { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public List<float> RZ { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public int NumOfObjects { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public ColorData ColorData { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public ObjectType ObjectType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
