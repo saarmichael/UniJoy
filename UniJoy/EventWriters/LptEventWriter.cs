1c3a0441d1997897fda38f2bca1e8d3278ab9cbc
avielkar <avraham.elkar@gmail.com>
2020-04-28 14:28:55 +0300

    Adding LptEventWriter and IEventWrite interface

---
+++ UniJoy/EventWriters/LptEventWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniJoy.EventWriters
+{
+    public class LptEventWriter : IEventWriter<int>
+    {
+        public void WriteEvent(int eventName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
