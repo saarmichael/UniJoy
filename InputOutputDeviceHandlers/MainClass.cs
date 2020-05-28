d317faea75a2131b525995a3c70803306e15d4f0
avielkar <avraham.elkar@gmail.com>
2020-05-28 19:31:38 +0300

    Adding the cedrus response logic

---
+++ InputOutputDeviceHandlers/MainClass.cs
@@ -0,0 +1,14 @@
+﻿using InputOutputDeviceHandlers.UserInputs.Cedrus;
+using System;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        CedrusResponseBox crb = new CedrusResponseBox();
+
+        crb.IsStartButtonPressed();
+
+        Console.ReadLine(); ;
+    }
+}
--- InputOutputDeviceHandlers/UserInputs/Cedrus/CedrusResponseBox.cs
+++ InputOutputDeviceHandlers/UserInputs/Cedrus/CedrusResponseBox.cs
@@ -2,21 +2,46 @@
 using System.IO.Ports;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.InteropServices;
+using System.Collections.Concurrent;
 
 namespace InputOutputDeviceHandlers.UserInputs.Cedrus
 {
     public class CedrusResponseBox : IUserInputController
     {
-        //private SerialPort
+        private SerialPort _serialPort;
+
+        private const int StartPress = 255;
+        private const int LeftPress = 254;
+        private const int RightPress = 253;
+        private const int DownPresss = 252;
+        private const int UpPress = 251;
+
+        public CedrusResponseBox()
+        {
+            _serialPort = new SerialPort("COM3",
+               9600, Parity.None, 8, StopBits.One);
+            
+            _serialPort.Open();
+        }
 
         public void FlushBuffer()
         {
-            throw new NotImplementedException();
+            _serialPort.BaseStream.Flush();
         }
 
         public bool IsStartButtonPressed()
         {
-            throw new NotImplementedException();
+            byte[] value = new byte[1];
+            if (_serialPort.BytesToRead > 0)
+            {
+                if (_serialPort.Read(value, 0, 1) == 1 && value[0] == StartPress)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public bool LoadButtonsMapping()
@@ -26,7 +51,28 @@ namespace InputOutputDeviceHandlers.UserInputs.Cedrus
 
         public PressType SubjectChoice()
         {
-            throw new NotImplementedException();
+            byte[] value = new byte[1];
+            if (_serialPort.BytesToRead > 0)
+            {
+                if (_serialPort.Read(value, 0, 1) == 1)
+                {
+                    switch (value[0])
+                    {
+                        case RightPress:
+                            return PressType.Right;
+                        case LeftPress:
+                            return PressType.Left;
+                        case UpPress:
+                            return PressType.Up;
+                        case DownPresss:
+                            return PressType.Down;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return PressType.None;
         }
     }
 }
