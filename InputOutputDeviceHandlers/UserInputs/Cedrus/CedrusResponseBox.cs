0b4d0f96c28d6382cc811d97b66db8add32dd46f
avielkar <avraham.elkar@gmail.com>
2020-05-28 10:45:16 +0300

    Adding new classes and namespaces

---
+++ InputOutputDeviceHandlers/UserInputs/Cedrus/CedrusResponseBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InputOutputDeviceHandlers.UserInputs.Cedrus
+{
+    public class CedrusResponseBox : IUserInputController
+    {
+        public void FlushBuffer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsStartButtonPressed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LoadButtonsMapping()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PressType SubjectChoice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
